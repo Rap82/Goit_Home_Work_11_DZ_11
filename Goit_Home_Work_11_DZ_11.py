63b9f5f0d8b981b2c758a62851dba33de3cb8f12
Rap82 <148592319+Rap82@users.noreply.github.com>
2024-02-04 17:44:23 +0200

    Task_9

--- Goit_Home_Work_11_DZ_11.py
+++ Goit_Home_Work_11_DZ_11.py
@@ -1120,7 +1120,117 @@
 
 # print(scalar) # 209
 
+
+
 # ================================ Звдання 9 / Task 9 ======================================
 
 # =================  9. Перевизначення математичних операторів. Реалізація методу довжини вектора *len(self)  ======================
 
+# ++++++++++++++++++++++++++++++++++++++  Умова / Condition ++++++++++++++++++++++++++++++++++++++++
+
+# Перш ніж ми приступимо до операцій порівняння векторів, реалізуйте метод визначення довжини вектора - len для класу Vector
+
+# Для вектора a з координатами (x1, y1) його довжина визначається за такою формулою:
+
+# (x1 ** 2 + y1 ** 2) ** 0.5.
+
+# Приклад коду:
+
+# vector1 = Vector(Point(1, 10))
+# vector2 = Vector(Point(10, 10))
+
+# print(vector1.len())  # 10.04987562112089
+# print(vector2.len())  # 14.142135623730951
+
+# +++++++++++++++++++++++++++++++++++++   Код / Code ++++++++++++++++++++++++++++++++++++++++
+
+# Примітка :  І з нового тут тільки реаліація методу *len - довжина векора. Все інше читай в попердніх завданнях.
+              # Все більше наш *class Vector стає схожий на звичайні вектори математичні.
+              
+
+class Point:
+    def __init__(self, x, y):
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if (type(x) == int) or (type(x) == float):
+            self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if (type(y) == int) or (type(y) == float):
+            self.__y = y
+
+    def __str__(self):
+        return f"Point({self.x},{self.y})"
+
+
+class Vector:
+    def __init__(self, coordinates: Point):
+        self.coordinates = coordinates
+
+    def __setitem__(self, index, value):
+        if index == 0:
+            self.coordinates.x = value
+        if index == 1:
+            self.coordinates.y = value
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.coordinates.x
+        if index == 1:
+            return self.coordinates.y
+
+    def __call__(self, value=None):
+        if value:
+            self.coordinates.x = self.coordinates.x * value
+            self.coordinates.y = self.coordinates.y * value
+        return self.coordinates.x, self.coordinates.y
+
+    def __add__(self, vector):
+        x = self.coordinates.x + vector.coordinates.x
+        y = self.coordinates.y + vector.coordinates.y
+        return Vector(Point(x, y))
+
+    def __sub__(self, vector):
+        x = self.coordinates.x - vector.coordinates.x
+        y = self.coordinates.y - vector.coordinates.y
+        return Vector(Point(x, y))
+
+    def __mul__(self, vector):
+        return (
+                self.coordinates.x * vector.coordinates.x
+                + self.coordinates.y * vector.coordinates.y
+        )
+
+    def len(self):
+        ''' Метод класу який обчислює довжину вектора(нашого екзепляру який має всі властивості вектора)
+        за формулою  len == (x1 ** 2 + y1 ** 2) ** 0.5
+        і повертає результат, число(тип *float або *int) , яке відповідає "довжині вектора" з параметрами вказаним в екзеплярі.'''
+
+        len_vector = (self.coordinates.x** 2 + self.coordinates.y ** 2) ** 0.5 # В математичну формулу обчислення довжини вектора , 
+                                                        #  піставляємо значення з полів *.coordinates.x i *.coordinates.y
+        return len_vector  #  Повертаємо довжину вектора з методу. 
+
+    def __str__(self):
+        return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+# +++++++++++++++++++++++++++++++++++++++++++ Тестові значення і виклик функції (не потрібний для автоперевірки.) +++++++
+    
+vector1 = Vector(Point(1, 10))
+vector2 = Vector(Point(10, 10))
+
+print(vector1.len())  # 10.04987562112089
+print(vector2.len())  # 14.142135623730951
